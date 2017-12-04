9253831581dda293eeb6756abd4366c1bf8c7aab
Noel Niles <noelniles@gmail.com>
2017-12-03 18:55:33 -1000

    Add tests for InvMod

--- zz.go
+++ zz.go
@@ -1,13 +1,35 @@
-// zz is the main package in NT (For now). It's just a bunch of functions over
+package main
+
+// zz is the package in NT (For now). It's just a bunch of functions over
 // the integers. There will probably be other packages or modules or something
 // that implement the real and complex stuff.
-package main
 
 import (
 	"fmt"
 	"math"
 )
 
+// RGCD is the recurisive gcd algorithm. This shouldn't be used.
+func RGCD(m int32, n int32) int32 {
+	if n == 0 {
+		return m
+	}
+	return RGCD(n, m%n)
+}
+
+// This is a slightly better GCD function, but it still
+// shouldn't be used because of the recursion.
+func RGCD2(a int32, b int32) int32 {
+	if a == b {
+		return a
+	} else if a > b {
+		return RGCD2(a-b, b)
+	} else {
+		return RGCD2(a, b-a)
+	}
+}
+
+// GCD is the Greatest Common Denominator of a and b.
 func GCD(a int32, b int32) int32 {
 	var u int32
 	var v int32
@@ -37,10 +59,11 @@ func GCD(a int32, b int32) int32 {
 	return x
 }
 
-// XGCD is the extended Euclidean Algorithm.
-func XGCD(a int32, b int32) (int32, int32, int32) {
-	var u, v, u0, v0, u1, v1, u2, v2, q, r int32
-	var aneg, bneg int32
+// XGCD is the extended Euclidean Algorithm. In addition to the GCD of a and b, XGCD computes the coefficients of
+// Bézouts identity x, y such that ax + by = gcd(a, b).
+func XGCD(a int64, b int64) (int64, int64, int64) {
+	var u, v, u0, v0, u1, v1, u2, v2, q, r int64
+	var aneg, bneg int64
 
 	if a < 0 {
 		if a < -math.MaxInt32 {
@@ -85,3 +108,20 @@ func XGCD(a int32, b int32) (int32, int32, int32) {
 	}
 	return u, u1, v1
 }
+
+// InvMod returns the multiplicative inverse of a mod n. Returns 0 if the inverse is undefined.
+func InvMod(a int64, n int64) int64 {
+	d, s, _ := XGCD(a, n)
+
+	if d != 1 {
+		fmt.Println("InvMod: inverse undefined")
+		return 0
+	}
+	if s < 0 {
+		return s + n
+	} else {
+		return s
+	}
+}
+
+func main() {}
--- zz_test.go
+++ zz_test.go
@@ -5,6 +5,43 @@ import (
 	"testing"
 )
 
+func TestRGCD(t *testing.T) {
+	if gcd := RGCD(13, 13); gcd != 13 {
+		t.Errorf("Expected GCD=13 but it was %d instead", gcd)
+	}
+	if gcd := RGCD(37, 600); gcd != 1 {
+		t.Errorf("Expected GCD=1 but it was %d instead", gcd)
+	}
+	if gcd := RGCD(20, 100); gcd != 20 {
+		t.Errorf("Expected GCD=6 but it was %d instead", gcd)
+	}
+	if gcd := RGCD(624129, 2061517); gcd != 18913 {
+		t.Errorf("Expected GCD=18913 but it was %d instead", gcd)
+	}
+	if gcd := RGCD(math.MaxInt32, 0); gcd != math.MaxInt32 {
+		t.Errorf("Expected GCD=%d but it was %d instead", math.MaxInt32, gcd)
+	}
+}
+
+func TestRGCD2(t *testing.T) {
+	if gcd := RGCD2(13, 13); gcd != 13 {
+		t.Errorf("Expected GCD=13 but it was %d instead", gcd)
+	}
+	if gcd := RGCD2(37, 600); gcd != 1 {
+		t.Errorf("Expected GCD=1 but it was %d instead", gcd)
+	}
+	if gcd := RGCD2(20, 100); gcd != 20 {
+		t.Errorf("Expected GCD=6 but it was %d instead", gcd)
+	}
+	if gcd := RGCD2(624129, 2061517); gcd != 18913 {
+		t.Errorf("Expected GCD=18913 but it was %d instead", gcd)
+	}
+	// This will  always fail because the recursion is too deep.
+	//if gcd := RGCD2(math.MaxInt32, 0); gcd != math.MaxInt32 {
+	//	t.Errorf("Expected GCD=%d but it was %d instead", math.MaxInt32, gcd)
+	//}
+}
+
 func TestGCD(t *testing.T) {
 	if gcd := GCD(13, 13); gcd != 13 {
 		t.Errorf("Expected GCD=13 but it was %d instead", gcd)
@@ -19,7 +56,7 @@ func TestGCD(t *testing.T) {
 		t.Errorf("Expected GCD=18913 but it was %d instead", gcd)
 	}
 	if gcd := GCD(math.MaxInt32, 0); gcd != math.MaxInt32 {
-		t.Errorf("Expected GCD=0 but it was %d instead", gcd)
+		t.Errorf("Expected GCD=%d but it was %d instead", math.MaxInt32, gcd)
 	}
 }
 
@@ -27,5 +64,31 @@ func TestXGCD(t *testing.T) {
 	if d, x, y := XGCD(4864, 3458); d != 38 && x != -45 && y != 38 {
 		t.Errorf("Expected XGCD=32,-45,38 but it was %d,%d,%d instead", d, x, y)
 	}
+}
 
+func TestInvMod(t *testing.T) {
+	if i := InvMod(1, 2); i != 1 {
+		t.Errorf("Expected InvMod(1, 2)=1 but it was %d instead", i)
+	}
+	if i := InvMod(3, 6); i != 0 {
+		t.Errorf("Expected InvMod(3, 6)=0 but it was %d instead", i)
+	}
+	if i := InvMod(7, 87); i != 25 {
+		t.Errorf("Expected InvMod(7, 87)=25 but it was %d instead", i)
+	}
+	if i := InvMod(2, 91); i != 46 {
+		t.Errorf("Expected InvMod(2, 91)=46 but it was %d instead", i)
+	}
+	if i := InvMod(13, 91); i != 0 {
+		t.Errorf("Expected InvMod(13, 91)=0 but it was %d instead", i)
+	}
+	if i := InvMod(13, 91); i != 0 {
+		t.Errorf("Expected InvMod(13, 91)=0 but it was %d instead", i)
+	}
+	if i := InvMod(31, 73714876143); i != 45180085378 {
+		t.Errorf("Expected InvMod(3, 7371487614)=45180085378 but it was %d instead", i)
+	}
+	if i := InvMod(3, 73714876143); i != 0 {
+		t.Errorf("Expected InvMod(3, 7371487614)=0 but it was %d instead", i)
+	}
 }
